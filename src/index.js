3b5b2b737d95d1e9638cad8a4dfb56885e7b81b5
StXt <stas.domanskiy@gmail.com>
2019-07-11 08:11:44 +0300

    checkFormData added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+/**************************** Import of styles and images *****************************/
+/**************************************************************************************/
 import './style.css';
-/*
 import cyclistPng from './images/cycling.png';
 import logoPng from './images/logo.png';
 import roadPng from './images/bikes/first.png';
 import trickPng from './images/bikes/second.png';
 import francePng from './images/competition/logo.png';
+import checkFormData from './checkFormData';
 
-
+/*********************** Adding images into needed places of html**********************/
+/**************************************************************************************/
 const cycleDiv = document.getElementsByClassName('cyclist')[0];
 const cyclist = new Image();
 cyclist.src = cyclistPng;
@@ -41,17 +44,15 @@ france.setAttribute('alt', 'Tour de France');
 france.classList.add('cycle-col__image');
 franceDiv.appendChild(france);
 
-const franceDiv = document.getElementsByClassName('tour-de-france')[0];
-const france = new Image();
-france.src = francePng;
-france.setAttribute('alt', 'Tour de France');
-france.classList.add('cycle-col__image');
-franceDiv.appendChild(france);*/
-
 const signUpBtn = document.querySelector('.sign-up .toggler__button'),
-      signInBtn = document.querySelector('.sign-in .toggler__button');
+signInBtn = document.querySelector('.sign-in .toggler__button');
 signUpBtn.addEventListener('click', toggleForm);
 signInBtn.addEventListener('click', toggleForm);
+
+/******************************** Form Handlers ***************************************/
+/**************************************************************************************/
+const users = [];
+
 
 function toggleForm(event) {
     event.preventDefault();
@@ -61,26 +62,25 @@ function toggleForm(event) {
     signIn.classList.toggle('active');
 }
 
-const users = [],
-      person = {};
-
-
-
-const signUpData = document.querySelector('.sign__container .form__button');
-signUpData.addEventListener('click', sendData);
-
 function sendData(event) {
     event.preventDefault();
-    const name = document.querySelectorAll('.sign__container .field__input')[0],
-        email = document.querySelectorAll('.sign__container .field__input')[1],
-        password = document.querySelectorAll('.sign__container .field__input')[2],
-        rePassword = document.querySelectorAll('.sign__container .field__input')[3];
+    
+    
+
+    /* Зібрати дані, пропустити через валідацію, сформувати юзера */
+    checkFormData(name, email, password, repeatedPassword)
+
     person.name = name.value;
     person.email = email.value;
     person.password = password.value;
     person.rePassword = rePassword.value;
+
+    console.log(person);
     users.push(person);
 }
+
+const signUpData = document.querySelector('.sign__container .form__button');
+signUpData.addEventListener('click', sendData);
 
 
 
