546cb63fe3961f1c6637e2578e26c516b0b7ffa8
Denys Khuda <Denys_Khuda@epam.com>
2019-07-11 16:43:28 +0300

    global fix

---
+++ src.temp/index.js
@@ -0,0 +1,84 @@
+/**************************** Import of styles and assets *****************************/
+/**************************************************************************************/
+import './style.css';
+import cyclistPng from '../assets/img/cycling.png';
+import logoPng from '../assets/img/logo.png';
+import roadPng from '../assets/img/bikes/first.png';
+import trickPng from '../assets/img/bikes/second.png';
+import francePng from '../assets/img/competition/logo.png';
+import checkFormData from './checkFormData';
+
+/*********************** Adding assets into needed places of html**********************/
+/**************************************************************************************/
+const cycleDiv = document.getElementsByClassName('cyclist')[0];
+const cyclist = new Image();
+cyclist.src = cyclistPng;
+cyclist.setAttribute('alt', 'Cyclist');
+cyclist.classList.add('bicycle');
+cycleDiv.appendChild(cyclist);
+
+const logoDiv = document.getElementsByClassName('navbar-logo')[0];
+const logo = new Image();
+logo.src = logoPng;
+logo.setAttribute('alt', 'Bikezilla logo');
+logoDiv.appendChild(logo);
+
+const roadDiv = document.getElementsByClassName('road-bike')[0];
+const roadBike = new Image();
+roadBike.src = roadPng;
+roadBike.setAttribute('alt', 'Road bike');
+roadBike.classList.add('image');
+roadDiv.appendChild(roadBike);
+
+const trickDiv = document.getElementsByClassName('trick-bike')[0];
+const trickBike = new Image();
+trickBike.src = trickPng;
+trickBike.setAttribute('alt', 'Trick bike');
+trickBike.classList.add('image');
+trickDiv.appendChild(trickBike);
+
+const franceDiv = document.getElementsByClassName('tour-de-france')[0];
+const france = new Image();
+france.src = francePng;
+france.setAttribute('alt', 'Tour de France');
+france.classList.add('cycle-col__image');
+franceDiv.appendChild(france);
+
+const signUpBtn = document.querySelector('.sign-up .toggler__button'),
+signInBtn = document.querySelector('.sign-in .toggler__button');
+signUpBtn.addEventListener('click', toggleForm);
+signInBtn.addEventListener('click', toggleForm);
+
+/******************************** Form Handlers ***************************************/
+/**************************************************************************************/
+const users = [];
+
+
+function toggleForm(event) {
+    event.preventDefault();
+    const signUp = document.querySelector('.sign-up'),
+          signIn = document.querySelector('.sign-in');
+    signUp.classList.toggle('active');
+    signIn.classList.toggle('active');
+}
+
+function sendData(event) {
+    event.preventDefault();
+
+    /* Зібрати дані, пропустити через валідацію, сформувати юзера */
+    checkFormData(name, email, password, repeatedPassword)
+
+    person.name = name.value;
+    person.email = email.value;
+    person.password = password.value;
+    person.rePassword = rePassword.value;
+
+    console.log(person);
+    users.push(person);
+}
+
+const signUpData = document.querySelector('.sign__container .form__button');
+signUpData.addEventListener('click', sendData);
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,98 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: {
-        app: './src/index.js',
+
+
+var config = {
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
     },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
-    },
+    entry: [
+        './src/index.js',
+    ],
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        // filename: './bundle[contenthash].js',
+        filename: './bundle.js',
+        path: path.resolve(__dirname, './dist'),
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        limit: 8000, // Convert assets < 8kb to base64 strings
+                        name: '[path][name].[ext]',
+                    }
+                }]
+            },
+            {
+                test: /\.(css|sass)$/,
+                use: ['style-loader', 'css-loader?url=false', 'sass-loader']
+            },
+            // {
+            //     test: /\.css$/,
+            //     use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            // },
+            {
+                test: /\.pug$/,
+                use: ["pug-loader"]
+            },
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
+        // new ExtractTextPlugin({
+        //     filename: './css/style.bundle.css',
+        //     allChunks: true,
+        // }),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html')
+            template: 'src/index.pug',
+            filename: 'index.html'
         }),
+        new CopyWebpackPlugin([
+            // {
+            //     from: './src/fonts',
+            //     to: './fonts'
+            // },
+            // {
+            //     from: './src/js/vendor',
+            //     to: './js/vendor'
+            // },
+            // {
+            //     from: './src/img',
+            //     to: './img'
+            // },
+        ]),
+        //new HtmlWebpackPugPlugin()
     ]
+};
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.devtool = 'source-map';
+        config.devServer = {
+            contentBase: './src'
+        }
+    }
+    if (argv.mode === 'production') {
+        config.devServer = {
+            contentBase: './dist'
+        }
+    }
+    return config;
 };
