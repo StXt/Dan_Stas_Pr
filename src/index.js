0baac1a5ed015bdf7bdaf10feb6b833e504c09f7
DanKhuda <dan.network.17@gmail.com>
2019-07-14 20:29:27 +0300

    import images added

--- src/index.js
+++ src/index.js
@@ -3,17 +3,7 @@ import './components/layouts/spinner/spinner'
 import './components/layouts/section1/section1'
 import "./index.pug"
 import  "./index.sass"
-import "../assets/img/bikezilla.png"
-import "../assets/img/cycling.png"
-import "../assets/img/background.jpg"
-import "../assets/img/bikes/first.png"
-import "../assets/img/bikes/second.png"
-import "../assets/img/competition/logo.png"
-import "../assets/img/competition/photo-1.jpg"
-import "../assets/img/social/facebook.png"
-import "../assets/img/social/google-plus-logo.png"
-import "../assets/img/social/instagram.png"
-import "../assets/img/social/twitter.png"
+import "../assets/img"
 
 const popUp = document.querySelector('.sign-up-modal');
 popUp.addEventListener('click', openModal);
