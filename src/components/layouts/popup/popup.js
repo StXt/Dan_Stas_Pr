908cc18c55e1f485adca115e313ecf8155bee8be
DanKhuda <dan.network.17@gmail.com>
2019-07-15 00:23:00 +0300

    classes added

--- src/components/layouts/popup/popup.js
+++ src/components/layouts/popup/popup.js
@@ -1,24 +1,28 @@
 class PopUp {
     constructor(btnOpen) {
         this.popUpBtn = document.querySelector(btnOpen);
-        this.closePopUpBtn = document.querySelector(".close");
-        this.popUpmodal = document.querySelector(".popup");
+        this.closePopUpBtn = document.querySelector(".btn-close");
     }
 
     init() {
         this.popUpBtn.addEventListener('click', this.toggleModal);
-        // this.closePopUpBtn.addEventListener('click', this.toggleModal);
+        this.closePopUpBtn.addEventListener('click', this.toggleModal);
+        window.addEventListener('click', this.closeModal);
     }
 
     toggleModal(event) {
         event.preventDefault();
+        this.popUpmodal = document.querySelector(".popup");
         this.popUpmodal.classList.toggle('active');
     }
 
-    /*closeModal(event) {
+    closeModal(event) {
         event.preventDefault();
-        this.popUpmodal.classList.toggle('active');
-    }*/
+        this.popUpmodal = document.querySelector(".popup");
+        if (event.target === this.popUpmodal) {
+            this.popUpmodal.classList.toggle('active');
+        }
+    }
 }
 
 export default PopUp;
