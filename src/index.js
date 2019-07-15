9f70e2c2b868b6e31b28413c7cd4358f647a6ae7
DanKhuda <dan.network.17@gmail.com>
2019-07-15 22:29:21 +0300

    global fix

--- src/index.js
+++ src/index.js
@@ -8,46 +8,11 @@ import "./index.pug"
 import  "./index.sass"
 import "../assets/img"
 import SignUp from "./components/layouts/signUp/signUp";
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
 
 let signUp = new SignUp(".sign-up-modal");
 signUp.init();
-
-/*const popUp = document.querySelector('.sign-up-modal');
-popUp.addEventListener('click', openModal);
-
-function openModal(event) {
-    event.preventDefault();
-    const signBlock = document.querySelector('.sign');
-    const wrapperBlock = document.querySelector('.wrapper');
-    signBlock.classList.toggle('active');
-}*/
-
-/*window.addEventListener('click', closeModal);
-
-function closeModal(event) {
-    const signBlock = document.querySelector('.sign');
-    console.log(event.target);
-    if (event.target === signBlock) {
-        signBlock.classList.add('hover');
-    }
-}*/
-
-/*
-const signUpBtn = document.querySelector('.sign-up .toggler__button'),
-      signInBtn = document.querySelector('.sign-in .toggler__button');
-signUpBtn.addEventListener('click', toggleForm);
-signInBtn.addEventListener('click', toggleForm);
-
-function toggleForm(event) {
-    event.preventDefault();
-    const signUp = document.querySelector('.sign-up'),
-          signIn = document.querySelector('.sign-in');
-    signUp.classList.toggle('active');
-    signIn.classList.toggle('active');
-}*/
-import * as firebase from 'firebase/app'
-
-import 'firebase/auth'
 
 var firebaseConfig = {
     apiKey: "AIzaSyD2Z_-c88rQn4FYrnVv7_eR5wOZbhWpuQ8",
@@ -58,7 +23,7 @@ var firebaseConfig = {
     messagingSenderId: "209338032365",
     appId: "1:209338032365:web:f62e43ebfceecdac"
 };
-// Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 (function () {
     firebase.auth().createUserWithEmailAndPassword("dan.network.17@gmail.com", "test123")
